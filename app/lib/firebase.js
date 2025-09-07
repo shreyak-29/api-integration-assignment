9703002fb75c6d84c6a7dd4f6d4a60d80e8e7ad5
Shreya Kathe <shreyakathe2904@gmail.com>
2025-09-07 22:53:40 +0530

    .

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -16,6 +16,5 @@ const googleProvider = new GoogleAuthProvider();
 console.log("Auth domain:", process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
 console.log("AuthDomain:", process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
 console.log("API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
-console.log("Current origin:", window.location.origin);
 
 export { auth, googleProvider };
